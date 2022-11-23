71fe444f813118d325ab78ae9c44be8e6bc11343
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2022-11-23 20:54:13 +0800

    update.

--- src/ForJLD2.jl
+++ src/ForJLD2.jl
@@ -82,3 +82,13 @@ function write_events_JLD2(file_name::String, event_list::Vector{Event})::Nothin
     save(file_name, file_content)
     return  nothing
 end
+
+function write_events_JLD2_new(file_name::String, event_list::Vector{Event})::Nothing
+    if splitext(file_name)[end] != ".jld2"
+        file_name   *=  ".jld2"
+    end
+
+    file_content    =   Symbol.("Event_" .* string.(eachindex(event_list))) .=> event_list
+    jldsave(file_name; file_content...)
+    return  nothing
+end
