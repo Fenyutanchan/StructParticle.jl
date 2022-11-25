f9801af97e7b3b73ac600331527054abaed69aba
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2022-11-25 23:05:31 +0800

    remove old functions.

--- src/StructParticle.jl
+++ src/StructParticle.jl
@@ -10,9 +10,6 @@ module StructParticle
     export  read_events_JLD2
     export  write_events_JLD2
 
-    export  read_events_JLD2_old
-    export  write_events_JLD2_old
-
     include("Particle.jl")
     include("ForJLD2.jl")
 
